83afe989c57105c6e59664674705825cee27811f
iAquino47 <ianaquino47@gmail.com>
2019-12-04 19:17:05 +0000

    a list of links on the page

--- app.rb
+++ app.rb
@@ -8,6 +8,5 @@ end
 
 post '/create_shortened_url' do
     shortened_url = Url.shorten(params[:original_url].to_s)
-    { shortened_url[0] => shortened_url[1] }.to_json
 	redirect to('/')
 end
---
+++ lib/url.rb
@@ -0,0 +1,19 @@
+class Url 
+    ID_LENGTH = 8
+
+    @links = {}
+    
+    def self.shorten(link)
+        unique_id = ([*('a'..'z'),*('0'..'9')]).sample(ID_LENGTH).join
+        if !link.start_with? "http"
+			link = "http://" + link
+		end
+
+        @links[unique_id] = link
+        [unique_id, link]
+    end
+
+    def self.links
+        @links
+    end
+end
