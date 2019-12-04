23d584c73deecc269dbd9d6c836b6089865ff510
iAquino47 <ianaquino47@gmail.com>
2019-12-04 18:15:58 +0000

    created a class with shorten method

--- app.rb
+++ app.rb
@@ -5,3 +5,9 @@ require 'json'
 get '/' do
     erb :index
 end
+
+post '/create_shortened_url' do
+    shortened_url = Url.shorten(params[:original_url].to_s)
+    { shortened_url[0] => shortened_url[1] }.to_json
+	redirect to('/')
+end
