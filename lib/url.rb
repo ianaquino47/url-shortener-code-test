4f920c960280e39e0d73d4928a105cacffe55e18
iAquino47 <ianaquino47@gmail.com>
2019-12-04 21:35:16 +0000

    checks for uniqueness of link to be shortened

--- lib/url.rb
+++ lib/url.rb
@@ -5,12 +5,15 @@ class Url
     @links = {}
     
     def self.shorten(link)
-        unique_id = ([*('a'..'z'),*('0'..'9')]).sample(ID_LENGTH).join
-
         #checks if user typed a link with http:// or https://
         if !link.start_with? "http" 
 			link = "http://" + link
-		end
+        end
+        
+        #checks if link has already been shortened
+        return [@links.key(link), link] if @links.has_value?(link)
+  
+        unique_id = ([*('a'..'z'),*('0'..'9')]).sample(ID_LENGTH).join
 
         #stores the original url along with its corresponding unique ID into the links hash
         @links[unique_id] = link
@@ -19,5 +22,9 @@ class Url
 
     def self.links
         @links
+    end
+
+    def link_unique
+        
     end
 end
